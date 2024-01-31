3ed8c4917673a41935e3ee70a3e02557f6f7f107
Albaryys <a.tily.pro@gmail.com>
2024-01-31 13:05:03 +0100

    change for another code

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,38 +1,30 @@
 using System;
 
-public class MatrixMath
+///<summary>Defines matrix calculation methods.</summary>
+class MatrixMath
 {
+    ///<summary>Multiplies two matrices.</summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        int rows1 = matrix1.GetLength(0);
-        int cols1 = matrix1.GetLength(1);
-        int rows2 = matrix2.GetLength(0);
-        int cols2 = matrix2.GetLength(1);
+        //Check if matrices can be multiplied
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return new double[,] { { -1 } };
+        
+        double[,] resultMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
 
-        // Check if matrices can be multiplied
-        if (cols1 != rows2)
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
         {
-            Console.WriteLine("Matrices cannot be multiplied.");
-            return new double[,] { { -1 } }; // Return a matrix containing -1
-        }
-
-        // Create the result matrix
-        double[,] result = new double[rows1, cols2];
-
-        // Perform matrix multiplication
-        for (int i = 0; i < rows1; i++)
-        {
-            for (int j = 0; j < cols2; j++)
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
             {
-                double sum = 0;
-                for (int k = 0; k < cols1; k++)
+                resultMatrix[i, j] = 0;
+
+                for (int k = 0; k < matrix1.GetLength(1); k++)
                 {
-                    sum += matrix1[i, k] * matrix2[k, j];
+                    resultMatrix[i, j] += matrix1[i, k] * matrix2[k, j];
                 }
-                result[i, j] = sum;
             }
         }
 
-        return result;
+        return resultMatrix;
     }
 }
