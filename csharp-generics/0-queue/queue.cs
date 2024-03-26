0944ccc14ce4b3c7db96f3550fe6aaf282003b68
Albaryys <a.tily.pro@gmail.com>
2024-03-26 12:48:01 +0100

    Refactor CheckType method in Queue class

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -4,12 +4,14 @@ using System;
 /// A generic queue class.
 /// </summary>
 /// <typeparam name="T">Type is user-defined at instantiation.</typeparam>
-class Queue<T> {
-    /// <summary>
-    /// Gets the type of the instance.
-    /// </summary>
-    /// <returns>Returns the instance type.</returns>
-    public Type CheckType() {
-        return typeof(T);
+class Queue<T>
+/// <summary>
+/// Gets the type of the instance.
+/// </summary>
+/// <returns>Returns the instance type.</returns>
+{
+    public string CheckType()
+    {
+        return typeof(T).ToString();
     }
 }
