7c9db17dc2400391abcf9f80069cb8c3d370a92c
Albaryys <a.tily.pro@gmail.com>
2024-04-04 15:17:35 +0200

    add task 0

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        // Properties
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+
+        // Constructor
+        public BaseClass()
+        {
+            // Set default values for date_created and date_updated
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
+
